78d4af26c30fd08bf049bce4cda59a5759f2150b
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2024-01-06 22:20:21 +0600

    sun category name done

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -9,30 +9,111 @@ import { Link } from "react-router-dom";
 const Category = () => {
     return (
         <div className=" flex justify-between text-lg items-center  mt-8 bg-green-400 text-orange-700 font-semibold px-3 py-2 rounded-lg ">
-            <Link to='/cat' className="hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg "><img className="items-center justify-items-center" src={kitty} alt="" />Cat</Link>
-            <Link to='/dog' className="hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg "><img src={dog} alt="" />Dog</Link>
-            <Link to='/bird' className="hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg"><img src={parrot} alt="" />Bird</Link>
-            <Link to='/fish' className="hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg"><img src={fish} alt="" />Fish</Link>
-            <Link to='/catfood' className="hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg"><img src={rabbit} alt="" />Rabbits</Link>
             
-            
-                     
-
-            <ul className="menu lg:menu-horizontal hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg ">
-                
+            <ul to='/cat' className="menu lg:menu-horizontal  hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg ">
             <li className="relative">
-                    <details open >
-                        <summary><img src={pills} alt="" />
-                        Medicine</summary>
-                        <ul className="bg-orange-400 ">
-                            <li><a>Cat Medicine</a></li>
-                            <li><a>Dog medicine</a></li>
-                            
+                    <details  >
+                        <summary><img className="items-center justify-items-center" src={kitty} alt="" />Cat</summary>
+                        <ul className="bg-orange-400  hover:text-orange-700 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Food</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Accessories</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Medicine</Link></li>
+
                         </ul>
                     </details>
-                </li>  
-               
+                </li>
             </ul>
+            
+            <ul to='/dog' className="menu lg:menu-horizontal  hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg ">
+            <li className="relative">
+                    <details >
+                        <summary><img src={dog} alt="" />Dog</summary>
+                        <ul className="bg-orange-400  hover:text-orange-700 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Food</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Accessories</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Medicine</Link></li>
+
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+            
+            <ul to='/bird' className="menu lg:menu-horizontal  hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg">
+            
+            <li className="relative">
+                    <details >
+                        <summary><img src={parrot} alt="" />Bird</summary>
+                        <ul className="bg-orange-400  hover:text-orange-700 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Food</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Accessories</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Medicine</Link></li>
+
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+           
+           
+           
+            <ul to='/fish' className="menu lg:menu-horizontal hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg">
+            
+            <li className="relative">
+                    <details >
+                        <summary><img src={fish} alt="" />Fish</summary>
+                        <ul className="bg-orange-400  hover:text-orange-700 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Food</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Accessories</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Medicine</Link></li>
+
+                        </ul>
+                    </details>
+                </li>
+            
+            
+            </ul>
+
+
+
+
+            <ul to='/catfood' className=" menu lg:menu-horizontal hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg">
+
+                <li className="relative">
+                    <details >
+                        <summary><img src={rabbit} alt="" />
+                        Rabbits</summary>
+                        <ul className="bg-orange-400  hover:text-orange-700 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Food</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Accessories</Link></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><Link>Medicine</Link></li>
+
+                        </ul>
+                    </details>
+                </li>
+
+            </ul>
+
+
+
+
+            <ul className="menu lg:menu-horizontal hover:border-2 border-orange-600 hover:text-green-700 hover:font-semibold px-3 py-2 rounded-lg ">
+
+                <li className="relative">
+                    <details  >
+                        <summary><img src={pills} alt="" />
+                            Medicine</summary>
+                        <ul className="bg-orange-400 ">
+                            <li className=" text-green-700 hover:text-orange-700 "><a>Cat Medicine</a></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><a>Dog medicine</a></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><a>Bird medicine</a></li>
+                            <li className=" text-green-700 hover:text-orange-700 "><a>Fish medicine</a></li>   
+                            <li className=" text-green-700 hover:text-orange-700 "><a>Rabbit medicine</a></li>   
+
+                        </ul>
+                    </details>
+                </li>
+
+            </ul>
+
 
 
 
