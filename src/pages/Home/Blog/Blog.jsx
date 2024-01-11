7f87e231897159362fe1d6cd851b303cb3994231
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2024-01-11 23:26:44 +0600

    blog slice done

--- src/pages/Home/Blog/Blog.jsx
+++ src/pages/Home/Blog/Blog.jsx
@@ -1,17 +1,39 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Assuming you're using React Router
+import BlogStyle from "./BlogStyle";
 
 const Blog = () => {
-    const [bolg, setBlog] = 
-    return (
-        <div>
-            <h1 className="text-3xl text-orange-400 font-bold text-center my-5 underline underline-offset-4">Our Blog</h1>
+  const [petBlog, setPetBlog] = useState([]);
 
+  useEffect(() => {
+    fetch('blog.json')
+      .then(res => res.json())
+      .then(data => setPetBlog(data))
+  }, []);
 
+  const visibleBlogs = petBlog.slice(0, 3);
 
+  return (
+    <div>
+      <h1 className="text-3xl text-orange-400 font-bold text-center my-5 underline underline-offset-4">Our Blog</h1>
 
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ml-6">
+        {
+          visibleBlogs.map(item => <BlogStyle
+            
+            key={item.id}
+            item={item}
+            ></BlogStyle>)
+        }
+      </div>
 
+      {petBlog.length > 3 && (
+        <div className="text-center mt-4">
+          <Link to="/see-more" className="bg-green-500 text-white font-semibold underline p-5 rounded-xl">See More</Link>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/pages/Home/Blog/BlogStyle.jsx
@@ -0,0 +1,25 @@
+
+
+const BlogStyle = ({ item }) => {
+
+    const { title, content } = item;
+
+    return (
+        <div className="card w-90 bg-gray-100 shadow-xl items-center justify-center">
+
+        {/* <figure className="px-6 pt-6">
+            <img src={image} alt="Shoes" className="rounded-xl" />
+        </figure> */}
+        <div className="card-body items-center text-center">
+                      
+            <h2 className="text-2xl font-bold text-amber-600">Title: {title} </h2>
+            <p className="text-gray-500 pt-2">{content}</p>
+            <div className="card-actions">
+                <button onClick={() => handleAddToCard(item)} className="btn btn-white bg-green-700 hover:bg-orange-600 font-bold border-none text-white mt-3">See More</button>
+            </div>
+        </div>
+    </div >
+    );
+};
+
+export default BlogStyle;
